81a6d02b0f5591c3fe4306950e49a7b66d1aa2b0
anildalar <dollorinfotech@gmail.com>
2023-05-18 17:31:34 +0530

    done

--- src/app/(layout2)/[city]/[category]/page.js
+++ src/app/(layout2)/[city]/[category]/page.js
@@ -1,7 +1,5 @@
-import React from 'react'
-
-export default function page() {
+export default function page({ params: { category,city } }) {
   return (
-    <div>City -/ Category - </div>
+    <div>City1 - { city } Category1 - {category}</div>
   )
 }
